03b45964494550d2e4b58e9fb0c41c19d57df301
Glieb <gleblitvinenko3007@gmail.com>
2023-07-27 18:12:23 +0300

    USER: changed users url

--- social_media_api/urls.py
+++ social_media_api/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/user/', include("user.urls", namespace="user")),
+    path('api/users/', include("user.urls", namespace="user")),
 
 ]
