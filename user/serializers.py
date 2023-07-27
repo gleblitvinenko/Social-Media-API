4959d047d29fd57ed2d74af0f0b8de43cc0e22a7
Glieb <gleblitvinenko3007@gmail.com>
2023-07-27 12:09:07 +0300

    USER: wrote UserRegisterSerializer for user registration

--- user/serializers.py
+++ user/serializers.py
@@ -1,5 +1,9 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+
+from user.models import User
 
 
 class MyProfileSerializer(serializers.ModelSerializer):
@@ -7,7 +11,15 @@ class MyProfileSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = get_user_model()
-        fields = ("id", "username", "email", "password", "bio", "profile_picture", "birth_date")
+        fields = (
+            "id",
+            "username",
+            "email",
+            "password",
+            "bio",
+            "profile_picture",
+            "birth_date",
+        )
         extra_kwargs = {"password": {"write_only": True, "min_length": 8}}
 
     def update(self, instance, validated_data):
@@ -17,5 +29,51 @@ class MyProfileSerializer(serializers.ModelSerializer):
         if password is not None:
             user.set_password(password)
             user.save()
+
+        return user
+
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        required=True, validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+
+    password = serializers.CharField(
+        write_only=True, required=True, validators=[validate_password]
+    )
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = (
+            "username",
+            "password",
+            "password2",
+            "email",
+            "first_name",
+            "last_name",
+        )
+        extra_kwargs = {
+            "first_name": {"required": True},
+            "last_name": {"required": True},
+        }
+
+    def validate(self, attrs):
+        if attrs["password"] != attrs["password2"]:
+            raise serializers.ValidationError(
+                {"password": "Password fields didn't match."}
+            )
+
+        return attrs
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            validated_data["username"],
+            validated_data["email"],
+            validated_data["password"],
+        )
+        user.first_name = validated_data["first_name"]
+        user.last_name = validated_data["last_name"]
+        user.save()
 
         return user
