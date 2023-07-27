bb79c69dea8d7e9e9fef55a28736204fa375e892
Glieb <gleblitvinenko3007@gmail.com>
2023-07-27 17:47:42 +0300

    USER: implemented user logout feature

--- user/urls.py
+++ user/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from user.views import MyProfileView, RegisterView, LoginView
+from user.views import MyProfileView, RegisterView, LoginView, LogoutView
 
 urlpatterns = [
     path("me/", MyProfileView.as_view(), name="my-profile"),
     path("register/", RegisterView.as_view(), name="auth-register"),
-    path('login/', LoginView.as_view(), name="auth-login"),
-
+    path("login/", LoginView.as_view(), name="auth-login"),
+    path("logout/", LogoutView.as_view(), name="auth-logout"),
 ]
 
 app_name = "user"
--- user/views.py
+++ user/views.py
@@ -1,8 +1,11 @@
 from rest_framework import generics
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.settings import api_settings
+from rest_framework.views import APIView
 
 from user.models import User
 from user.permissions import AllowUnauthenticatedOnly
@@ -29,3 +32,13 @@ class LoginView(ObtainAuthToken):
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
     serializer_class = CustomAuthTokenSerializer
     permission_classes = (AllowUnauthenticatedOnly,)
+
+
+class LogoutView(APIView):
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    @staticmethod
+    def get(request):
+        Token.objects.filter(user=request.user).delete()
+        return Response({"detail": "Successfully logged out."})
