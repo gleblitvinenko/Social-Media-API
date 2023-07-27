869a6f10c031d729a61392f31ff62dff453fa75b
Glieb <gleblitvinenko3007@gmail.com>
2023-07-27 12:25:07 +0300

    USER: refactored registration fields order

--- user/serializers.py
+++ user/serializers.py
@@ -46,10 +46,10 @@ class UserRegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = (
-            "username",
+            "email",
             "password",
             "password2",
-            "email",
+            "username",
             "first_name",
             "last_name",
         )
