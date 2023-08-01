52074e24f2124017bdd199a1f60fb8163b0de1cd
Glieb <gleblitvinenko3007@gmail.com>
2023-08-01 13:51:04 +0300

    Added HOW TO RUN section to README.md & hide SECRET_KEY

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -9,18 +9,20 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-t(gnmki8e7hfl83!xe2)y!63&w!v8_ctt2v(b6c&fa&bdgl^=&"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
